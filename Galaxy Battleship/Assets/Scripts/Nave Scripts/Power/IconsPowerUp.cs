b43c86d665913eaac84013152f42a5cc420404d1
leoonn <flateia13@hotmail.com>
2020-09-07 20:15:04 -0300

    icones para o canvas
    icones para o canvas

---
+++ Galaxy Battleship/Assets/Scripts/Nave Scripts/Power/IconsPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconsPowerUp : MonoBehaviour
+{
+
+    public GameObject [] iconPowerUp;
+    public GameObject[] positionIcon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
