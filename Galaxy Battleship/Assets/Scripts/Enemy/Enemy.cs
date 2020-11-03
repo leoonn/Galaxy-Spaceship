ad6e173ff1309866e0e3401fdafc6ae19f4740b7
leoonn <flateia13@hotmail.com>
2020-11-03 19:34:33 -0300

    falta terminar o duplicate

--- Galaxy Battleship/Assets/Scripts/Enemy/Enemy.cs
+++ Galaxy Battleship/Assets/Scripts/Enemy/Enemy.cs
@@ -14,11 +14,19 @@ public class Enemy : MonoBehaviour
 
     public typeEnemy type;
     public int lifeEnemy;
+
+    SpawnDuplicate spawnDuplicateScript;
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         PlayerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<move>();
         pointsScript = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Pontuation>();
+
+
+        if (type == typeEnemy.duplicate)
+        { 
+           // spawnDuplicateScript = 
+        }
     }
 
     // Update is called once per frame
@@ -32,8 +40,15 @@ public class Enemy : MonoBehaviour
                 RotatePlayer(playerTransform.position);
                 break;
             case typeEnemy.dead:
+                if (lifeEnemy <= 0)
+                {
+                    lifeEnemy = 0;
+                 
+                    Destroy(gameObject); //destroy asteroid
 
-                lifeEnemy = 0;
+                    pointsScript.points = pointsScript.points + pointsScript.ChasePoints;
+                    pointsScript.textscore.text = pointsScript.points.ToString();
+                }
                 GameObject explosionprefab = Instantiate(explosion, transform.position, transform.rotation); // instantieate a particle system
                 Destroy(gameObject);
                 break;
@@ -66,11 +81,8 @@ public class Enemy : MonoBehaviour
             GameObject explosionprefab = Instantiate(explosion, col.transform.position, transform.rotation); // instantieate a particle system
             Destroy(explosionprefab, 3f); //destroy particle
 
-            Destroy(col.gameObject); //destroy bullet       
-            Destroy(gameObject); //destroy asteroid
-
-            pointsScript.points = pointsScript.points + pointsScript.ChasePoints;
-            pointsScript.textscore.text = pointsScript.points.ToString();
+            Destroy(col.gameObject); //destroy bullet    
+            
         }
 
         if (col.gameObject.CompareTag("Player"))
@@ -79,5 +91,11 @@ public class Enemy : MonoBehaviour
             PlayerScript.PlayerDamage();
 
         }
+    }
+
+    void LifeEnemy()
+    { 
+        
+
     }
 }
---
+++ Galaxy Battleship/Assets/Scripts/Enemy/SpawnDuplicate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDuplicate : MonoBehaviour
+{
+    public GameObject duplicated;
+    public GameObject duplicated2;
+
+    
+    void Start()
+    {
+      
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateMinis()
+    { 
+        
+            Instantiate(duplicated, gameObject.transform.position, gameObject.transform.rotation);
+            
+        
+    
+    }
+}
